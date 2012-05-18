7512d202ad5d3af7e9ccd3a0aa6fdc09483a656a
Stephen McIntosh <stephenmac7@gmail.com>
2012-05-17 22:13:12 -0400

    Major Updates

--- ffpy.py
+++ ffpy.py
@@ -3,8 +3,10 @@
 # A rewrite of ffpy using the pyside toolkit
 # Imports
 import sys
+from getpass import getuser as username
 from PySide.QtGui import *
 from PySide.QtCore import *
+from frames import *
 
 # Pick either audio or video
 video = False
@@ -50,11 +52,58 @@ class AudioVideo(QWidget):
     qr.moveCenter(cp)
     self.move(qr.topLeft())
 
+class Audio(QWidget):
+  def __init__(self):
+    super(Audio, self).__init__()
+    self.initUI()
+
+  def initUI(self):
+    # Create Frames
+    fileframe = FileFrame()
+    audioframe = AFrame()
+    # Create Splitters
+    fa_split = QSplitter(Qt.Horizontal)
+    fa_split.addWidget(fileframe)
+    fa_split.addWidget(audioframe)
+    # Create layout
+    grid = QGridLayout()
+    grid.addWidget(fa_split, 0, 0)
+    self.setLayout(grid)
+
+class mainApp(QMainWindow):
+  def __init__(self):
+    super(mainApp, self).__init__()
+    self.initUI()
+
+  def initUI(self):
+    # Get Main Widget
+    global video
+    if video:
+      self.mwidget = Video()
+      self.setWindowTitle("ffpy Video")
+    else:
+      self.mwidget = Audio()
+      self.setWindowTitle("ffpy Audio")
+    self.setCentralWidget(self.mwidget)
+    # Create statusbar
+    status = self.statusBar()
+    status.showMessage("Idle.")
+
+    self.center()
+    self.show()
+
+  def center(self):
+    qr = self.frameGeometry()
+    cp = QDesktopWidget().availableGeometry().center()
+    qr.moveCenter(cp)
+    self.move(qr.topLeft())
+
 def main():
   app = QApplication(sys.argv)
   ex = AudioVideo()
   app.exec_()
-  print(video)
+  ex = mainApp()
+  sys.exit(app.exec_())
 
 if __name__ == '__main__':
   main()
---
+++ frames.py
@@ -0,0 +1,74 @@
+from PySide.QtGui import *
+from PySide.QtCore import *
+
+class FileFrame(QFrame):
+  """ File Options """
+  def __init__(self):
+    super(FileFrame, self).__init__()
+    self.initUI()
+
+  def initUI(self):
+    # Create grid layout
+    filegrid = QGridLayout()
+    filegrid.setSpacing(10)
+    # Create Labels/Selectors
+    input_btn = QPushButton("Input File")
+    input_btn.clicked.connect(self.getFile)
+    output_btn = QPushButton("Output File")
+    output_btn.clicked.connect(self.getFile)
+    # Create Entries
+    self.input_ent = QLineEdit(self)
+    self.output_ent = QLineEdit(self)
+    # Layout
+    filegrid.addWidget(input_btn, 0, 0)
+    filegrid.addWidget(output_btn, 1, 0)
+    filegrid.addWidget(self.input_ent, 0, 1, 1, 1)
+    filegrid.addWidget(self.output_ent, 1, 1, 2, 1)
+    self.setLayout(filegrid)
+
+  def getFile(self):
+    default_path = "/home/" + username()
+    if self.sender().text() == "Input File":
+      fname = QFileDialog.getOpenFileName(self, "Input File", default_path)[0]
+      self.input_ent.clear()
+      self.input_ent.insert(fname)
+    elif self.sender().text() == "Output File":
+      fname = QFileDialog.getSaveFileName(self, "Output File", default_path)[0]
+      self.output_ent.clear()
+      self.output_ent.insert(fname)
+
+  @property
+  def fileInfo(self):
+    return input_ent.text(), output_ent.text()
+
+class AFrame(QFrame):
+  """ Audio Options """
+  def __init__(self):
+    super(AFrame, self).__init__()
+    self.initUI()
+
+  def initUI(self):
+    # Create grid layout
+    agrid = QGridLayout()
+    agrid.setSpacing(10)
+    # Create Labels/Boxes For ab and sr
+    agrid.addWidget(QLabel("Audio Bitrate", self), 0, 0)
+    agrid.addWidget(QLabel("Sample Rate (in Hz)", self), 1, 0)
+    self.abr_ent = QLineEdit(self)
+    self.asr_ent = QLineEdit(self)
+    agrid.addWidget(self.abr_ent, 0, 1)
+    agrid.addWidget(self.asr_ent, 1, 1)
+    # Create Audio Codec Stuff
+    acodecs = ["libvorbis", "libmp3lame", "faac"]
+    self.acodec_combo = QComboBox(self)
+    for acodec in acodecs:
+      self.acodec_combo.addItem(acodec)
+    agrid.addWidget(QLabel("Audio Codec", self), 2, 0)
+    agrid.addWidget(self.acodec_combo, 2, 1)
+    # Set layout
+    self.setLayout(agrid)
+
+  @property
+  def audioInfo(self):
+    return self.abr_ent.text(), self.asr.text(), self.acodec_combo.text()
+
